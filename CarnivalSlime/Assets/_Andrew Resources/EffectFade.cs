e41fd4901d03f6734035e1b46cb9271c8b39f614
Unknown <atd335@nyu.edu>
2020-11-25 14:08:51 -0500

    hoop tutorial added (read below for the rest)
    face added
    juice framework added
    code for siena's particle effect added
    CrowdMeter UI Gameobject added (integrated into the spacebar)

---
+++ CarnivalSlime/Assets/_Andrew Resources/EffectFade.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectFade : MonoBehaviour
+{
+    Vector3 startPos;
+    Vector3 endPos;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        endPos = startPos + Vector3.up*1.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, endPos+Vector3.up*.1f, Time.deltaTime * 3);
+        transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one*3,Time.deltaTime * 3.3f);
+        GetComponent<SpriteRenderer>().color = Color.Lerp(GetComponent<SpriteRenderer>().color, new Color(1,1,1,0), Time.deltaTime * 3.3f);
+        if (transform.position.y>endPos.y)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
